a7f8b0a1c6dcff1edaa3c8b7aa5ed50f879b3d9e
Leonardo Montoya <leonardo.montoya@utec.edu.pe>
2023-10-28 21:56:19 -0500

    Corrección en repositorio CourseAssessmentDetailsRepository

--- demo/src/main/java/com/example/demo/CourseAssessmentDetails/repository/CourseAssessmentDetails.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.CourseAssessmentDetails.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseAssessmentDetails extends JpaRepository<CourseAssessmentDetails,Long> {
-}
